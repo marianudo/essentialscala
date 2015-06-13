2c30d6f836842f021ef0833aa049762dd2f5a173
Mariano Navas <marianudo@gmail.com>
2015-06-14 00:36:57 +0200

    Calculator feature with some operations

---
+++ src/main/scala/Calculator.scala
@@ -0,0 +1,50 @@
+sealed trait Expression {
+  def eval: Calculation
+}
+
+final case class Addition(left: Expression, right: Expression) extends Expression {
+  def eval = left.eval match {
+    case Failure(msg) => Failure(msg)
+    case Success(lr) => right.eval match {
+      case Failure(msg) => Failure(msg)
+      case Success(rr) => Success(lr + rr)
+    }
+  }
+}
+
+final case class Substraction(left: Expression, right: Expression) extends Expression {
+  def eval = left.eval match {
+    case Failure(msg) => Failure(msg)
+    case Success(lr) => right.eval match {
+      case Failure(msg) => Failure(msg)
+      case Success(rr) => Success (lr - rr)
+    }
+  }
+
+}
+
+final case class Division(left: Expression, right: Expression) extends Expression {
+  def eval = left.eval match {
+    case Failure(msg) => Failure(msg)
+    case Success(lr) => right.eval match {
+      case Failure(msg) => Failure(msg)
+      case Success(rr) => Success(lr / rr)
+    }
+  }
+}
+
+final case class SquareRoot(value: Double) extends Expression {
+  def eval =
+    if(value < 0) Failure("Square root of negative number")
+    else Success(math.pow(value, 0.5))
+}
+
+final case class Number(value: Double) extends Expression {
+  def eval = Success(value)
+}
+
+sealed trait Calculation
+
+final case class Success(result: Double) extends Calculation
+
+final case class Failure(message: String) extends Calculation
