135ee8f7d938ef54e92fb906f7cf34f1aff29816
Mariano Navas <marianudo@gmail.com>
2015-06-15 16:30:01 +0200

    Implement fold for Sum (Either) type

--- src/main/scala/Generics.scala
+++ src/main/scala/Generics.scala
@@ -2,7 +2,12 @@ package com.codinginflipflops.essentialscala.generics
 
 final case class Pair[A,B](one: A, two: B)
 
-sealed trait Sum[A,B]
+sealed trait Sum[A,B] {
+  def fold[C](left: A => C)(right: B => C): C = this match {
+    case Left(x) => left(x)
+    case Right(x) => right(x)
+  }
+}
 
 final case class Left[A,B](value: A) extends Sum[A,B]
 
