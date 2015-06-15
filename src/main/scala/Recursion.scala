3d71a9544fb146754ab4f0e11f08ba37d78c0c8e
Mariano Navas <marianudo@gmail.com>
2015-06-15 10:27:15 +0200

    Remove exceptions from apply method

--- src/main/scala/Recursion.scala
+++ src/main/scala/Recursion.scala
@@ -39,18 +39,22 @@ sealed trait LinkedList[A] {
     case LinkedPair(hd, tl) => hd == a || (tl contains a)
   }
 
-  def apply(n: Int): A = {
-    @annotation.tailrec
-    def go(n: Int, remaining: LinkedList[A]): A = remaining match {
-      case LinkedPair(hd, tl) if(n==0) => hd
-      case LinkedPair(hd, tl) => go(n -1, tl)
-      case LinkedEnd() => throw new NoSuchElementException()
+  @annotation.tailrec
+  def apply(n: Int): LinkedListResult[A] = this match {
+    case LinkedPair(hd, tl) => {
+      if(n==0) LinkedListSuccess(hd)
+      else tl(n - 1)
     }
-    go(n, this)
+    case LinkedEnd() => LinkedListFailure("No such element")
   }
-
 }
 
 final case class LinkedEnd[A]() extends LinkedList[A]
 
 final case class LinkedPair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
+
+sealed trait LinkedListResult[A]
+
+final case class LinkedListSuccess[A](result: A) extends LinkedListResult[A]
+
+final case class LinkedListFailure[A](reason: String) extends LinkedListResult[A]
