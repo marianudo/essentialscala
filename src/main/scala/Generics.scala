1df401792d133460c5a118a0c3afd4ea1b4f485f
Mariano Navas <marianudo@gmail.com>
2015-06-16 10:02:19 +0200

    map and flatMap in Sum[A,B] type

--- src/main/scala/Generics.scala
+++ src/main/scala/Generics.scala
@@ -4,11 +4,25 @@ final case class Pair[A,B](one: A, two: B)
 
 sealed trait Sum[A,B] {
   def fold[C](left: A => C)(right: B => C): C = this match {
-    case Left(x) => left(x)
-    case Right(x) => right(x)
+    case SumFailure(x) => left(x)
+    case SumSuccess(x) => right(x)
   }
+
+  def map[C](f: B => C): Sum[A,C] = this match {
+    case SumSuccess(v) => SumSuccess(f(v))
+    case SumFailure(msg) => SumFailure(msg)
+  }
+
+  def flatMap[C](f: B => Sum[A,C]): Sum[A,C] = this match {
+    case SumSuccess(v) => f(v)
+    case SumFailure(msg) => SumFailure(msg)
+  }
+
+  def mapViaFlatMap[C](f: B => C): Sum[A,C] =
+    flatMap(x => SumSuccess(f(x)))
+
 }
 
-final case class Left[A,B](value: A) extends Sum[A,B]
+final case class SumFailure[A,B](value: A) extends Sum[A,B]
 
-final case class Right[A,B](value: B) extends Sum[A,B]
+final case class SumSuccess[A,B](value: B) extends Sum[A,B]
