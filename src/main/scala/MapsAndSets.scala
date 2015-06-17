809857e3229a74743ee45d0ff0bb3e37c762c199
Mariano Navas <marianudo@gmail.com>
2015-06-17 15:04:18 +0200

    Playing with sets and maps in a functional way

---
+++ src/main/scala/MapsAndSets.scala
@@ -0,0 +1,53 @@
+package com.codinginflipflops.essentialscala
+
+object MapsAndSets {
+  val people = Set(
+    "Alice",
+    "Bob",
+    "Charlie",
+    "Derek",
+    "Edith",
+    "Fred"
+  )
+
+  val ages = Map(
+    "Alice" -> 10,
+    "Bob" -> 30,
+    "Charlie" -> 50,
+    "Derek" -> 40,
+    "Edith" -> 10,
+    "Fred" -> 60
+  )
+
+  val favoriteColors = Map(
+    "Bob" -> "green",
+    "Derek" -> "magenta",
+    "Fred" -> "yellow"
+  )
+
+  val favoriteLolCats = Map(
+    "Alice" -> "Long cat",
+    "Charlie" -> "Ceiling Cat",
+    "Edith" -> "Cloud Cat"
+  )
+
+  def favoriteColor(name: String): String =
+    favoriteColors.get(name).getOrElse("")
+
+  def printColors = {
+    favoriteColors foreach {a => println(a._2)}
+  }
+
+  def lookup[A](name: String, map: Map[String, A]): Option[A] =
+    map get name
+
+  val oldestColor = {
+    val agesList = for {
+      ageEntry <- ages
+    } yield ageEntry._2
+
+    val maxAge = agesList.max
+    val oldestGuyAgeEntry = ages.find { case (a, b) => b == maxAge}
+    favoriteColor(oldestGuyAgeEntry.get._1)
+  }
+}
